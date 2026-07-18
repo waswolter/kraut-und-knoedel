96a6e510fa41870dec83f6d3f3217e2bc6f41f33
waswolter <waswolter@gmail.com>
2026-07-18 23:33:33 +0200

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,34 @@
+const CACHE = 'kk-v1';
+const PRECACHE = ['/', '/index.html', '/manifest.json', '/icon.svg'];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE).then(c => c.addAll(PRECACHE)).then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    ).then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', e => {
+  // Lass API-Aufrufe immer durch (kein Cache)
+  if (e.request.url.includes('/api/')) return;
+
+  e.respondWith(
+    caches.match(e.request).then(cached => {
+      const network = fetch(e.request).then(res => {
+        if (res.ok) {
+          const clone = res.clone();
+          caches.open(CACHE).then(c => c.put(e.request, clone));
+        }
+        return res;
+      });
+      return cached || network;
+    })
+  );
+});
